442ff7ff7ed7a33652410690e844a5266eec648a
Jndungue089 <jndungue@gmail.com>
2026-09-21 21:48:21 +0100

    apk route done

---
+++ src/app/apk/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.redirect('https://drive.google.com/file/d/1h_VPzgXpJ0XadYBZO5CSY6jWDzytH2fp/view?usp=sharing');
+}
